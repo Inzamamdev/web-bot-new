48a25e387cad993ec71a7b536bc47727f07fa75d
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-08-14 10:06:50 +0530

    dropdown list for commands 6

--- src/telegram_bot/services/dispatcher.py
+++ src/telegram_bot/services/dispatcher.py
@@ -10,8 +10,8 @@ def register_commands(app):
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("login", login_command))
     app.add_handler(CommandHandler("logout", logout_command))
-    app.add_handler(CommandHandler("selectRepo", select_repo_command))
-    app.add_handler(CommandHandler("currentRepo", current_repo_command))
+    app.add_handler(CommandHandler("selectrepo", select_repo_command))
+    app.add_handler(CommandHandler("currentrepo", current_repo_command))
     app.add_handler(CallbackQueryHandler(select_repo_callback, pattern="^select_repo:"))
     app.add_handler(CallbackQueryHandler(select_branch_callback, pattern="^select_branch:"))
     
